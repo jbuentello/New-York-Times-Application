d5ad37f8e45f5c28c523d94e2c96396fecb29fa7
Austin Graves <austingraves106@aol.com>
2019-08-24 12:59:46 -0500

    Fixing the collab issue. Here is all the code to this point

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,39 @@
-console.log('Hello');
+const API_KEY = 'kJOiAvWAUX8EQeuLZVl9PP1vXBYxhkIw'
+
 let searchTerm = $('#searchTerm');
 let numbersToRetrive = $('#recordsToRetrieve');
 let startYear = $('#startYear');
 let endYear = $('#endYear');
+
+const queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTerm}&api-key=${API_KEY}`;
+
+function clearTheInputs() {
+  searchTerm = '';
+  numbersToRetrive = '';
+  startYear = '',
+  endYear = ''
+};
+
+function searchForResults() {
+// Testing for Inputs
+  console.log(searchTerm);
+  console.log(numbersToRetrive);
+  console.log(startYear);
+  console.log(endYear);
+// ------------------------
+// Ajax parameters for api call.
+  let options = {
+    url: queryURL,
+    method: 'GET'
+  }
+
+ // Actual Ajax call 
+  $.ajax(options).then(function(response){
+    console.log(response);
+  });
+}
+
+
+
+$('#search').on('click', searchForResults);
+$('#clear').on('click', clearTheInputs);
